7a898ff8b7803cad940c6f7c08a63e6e59b4c883
Difei Gu <gudifeifriend@gmail.com>
2020-05-29 20:01:23 -0400

    Finished functionality

--- lab1/a0_starter/CCServer.java
+++ lab1/a0_starter/CCServer.java
@@ -1,6 +1,8 @@
 import javax.xml.crypto.Data;
 import java.io.*;
+import java.lang.reflect.Array;
 import java.net.*;
+import java.util.*;
 
 class CCServer {
     public static void main(String args[]) throws Exception {
@@ -14,63 +16,69 @@ class CCServer {
 	System.out.println("listening on port " + port);
 	while(true) {
 	    try {
-		/*
-		  YOUR CODE GOES HERE
-		  - accept connection from server socket
-		  - read requests from connection repeatedly
-		  - for each request, compute an output and send a response
-		  - each message has a 4-byte header followed by a payload
-		  - the header is the length of the payload
-		    (signed, two's complement, big-endian)
-		  - the payload is a string
-		    (UTF-8, big-endian)
-		*/
-		Socket csock = ssock.accept();
-		System.out.println("Just connect to " + csock.getRemoteSocketAddress());
-		DataInputStream din = new DataInputStream(csock.getInputStream());
-		// read the first four bytes of the input stream, which indicates the size of the payload
-		final int size = din.readInt();
-		System.out.println("The size of the data payload is: " + size);
-		final byte[] inBytes = new byte[size];
-		din.readFully(inBytes);
+			/*
+			  YOUR CODE GOES HERE
+			  - accept connection from server socket
+			  - read requests from connection repeatedly
+			  - for each request, compute an output and send a response
+			  - each message has a 4-byte header followed by a payload
+			  - the header is the length of the payload
+				(signed, two's complement, big-endian)
+			  - the payload is a string
+				(UTF-8, big-endian)
+			*/
+			Socket csock = ssock.accept();
+			System.out.println("Just connect to " + csock.getRemoteSocketAddress());
+			DataInputStream din = new DataInputStream(csock.getInputStream());
+			// read the first four bytes of the input stream, which indicates the size of the payload
+			final int size = din.readInt();
+			System.out.println("The size of the data payload is: " + size);
+			final byte[] inBytes = new byte[size];
+			din.readFully(inBytes);
 
-		UnionFind graph = new UnionFind();
+			TriangleEnumeration graph = new TriangleEnumeration();
 
-		int i = 0;
-		while(i < inBytes.length){
-			int edge_i = 0;
-			while(inBytes[i] != 32){
-				char c = (char) inBytes[i];
-				edge_i = edge_i * 10 + Character.getNumericValue(c);
+			int i = 0;
+			while(i < inBytes.length){
+				int edge_i = 0;
+				while(inBytes[i] != 32){
+					char c = (char) inBytes[i];
+					edge_i = edge_i * 10 + Character.getNumericValue(c);
+					i++;
+				}
 				i++;
-			}
-			i++;
 
-			int edge_j = 0;
-			while(inBytes[i] != 10){
-				char c = (char) inBytes[i];
-				edge_j = edge_j * 10 + Character.getNumericValue(c);
+				int edge_j = 0;
+				while(inBytes[i] != 10){
+					char c = (char) inBytes[i];
+					edge_j = edge_j * 10 + Character.getNumericValue(c);
+					i++;
+				}
 				i++;
+
+				graph.insert(edge_i, edge_j);
 			}
-			i++;
 
-			graph.union(edge_i, edge_j);
-		}
 
-		DataOutputStream dout = new DataOutputStream(csock.getOutputStream());
-		StringBuilder output = new StringBuilder();
-		for(int edge: graph.keySet()){
-			output.append(edge).append(" ").append(graph.find(edge)).append("\n");
-		}
+			DataOutputStream dout = new DataOutputStream(csock.getOutputStream());
+			StringBuilder output = new StringBuilder();
 
-		byte[] response = output.toString().getBytes();
-		dout.writeInt(response.length);
-		dout.write(response);
-		dout.flush();
-		dout.close();
+			ArrayList<String> solution = graph.enumerate();
+
+			Collections.sort(solution);
+
+			for(String val : solution) {
+				output.append(val).append("\n");
+			}
+
+			byte[] response = output.toString().getBytes();
+			dout.writeInt(response.length);
+			dout.write(response);
+			dout.flush();
+			dout.close();
 
 	    } catch (Exception e) {
-		e.printStackTrace();
+			e.printStackTrace();
 	    }
 	}
     }
