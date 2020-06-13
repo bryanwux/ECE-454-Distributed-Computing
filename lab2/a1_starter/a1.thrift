aac1f04a83e01395242d4986e5499ae18bd005d6
Jiayu <j285wu@uwaterloo.ca>
2020-06-13 19:25:19 -0400

    add some stuff

--- lab2/a1_starter/a1.thrift
+++ lab2/a1_starter/a1.thrift
@@ -5,4 +5,5 @@ exception IllegalArgument {
 service BcryptService {
  list<string> hashPassword (1: list<string> password, 2: i16 logRounds) throws (1: IllegalArgument e);
  list<bool> checkPassword (1: list<string> password, 2: list<string> hash) throws (1: IllegalArgument e);
+ void BENodeHandler(1: string BEHost, 2: i32 BEPort) throws (1:IllegalArgument e);
 }
