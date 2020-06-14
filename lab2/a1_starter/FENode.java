a8bd5dc7edb63447df4c3ab0f94075de0ea88974
Difei Gu <gudifeifriend@gmail.com>
2020-06-14 13:09:30 -0400

    modified client and FE to perform client FE interaction

--- lab2/a1_starter/FENode.java
+++ lab2/a1_starter/FENode.java
@@ -17,26 +17,26 @@ public class FENode {
     static Logger log;
 
     public static void main(String [] args) throws Exception {
-	if (args.length != 1) {
-	    System.err.println("Usage: java FENode FE_port");
-	    System.exit(-1);
-	}
+		if (args.length != 1) {
+			System.err.println("Usage: java FENode FE_port");
+			System.exit(-1);
+		}
 
-	// initialize log4j
-	BasicConfigurator.configure();
-	log = Logger.getLogger(FENode.class.getName());
+		// initialize log4j
+		BasicConfigurator.configure();
+		log = Logger.getLogger(FENode.class.getName());
 
-	int portFE = Integer.parseInt(args[0]);
-	log.info("Launching FE node on port " + portFE);
+		int portFE = Integer.parseInt(args[0]);
+		log.info("Launching FE node on port " + portFE);
 
-	// launch Thrift server
-	BcryptService.Processor processor = new BcryptService.Processor<BcryptService.Iface>(new BcryptServiceHandler());
-	TNonblockingServerSocket socket = new TNonblockingServerSocket(portFE);
-	THsHaServer.Args sargs = new THsHaServer.Args(socket);
-	sargs.protocolFactory(new TBinaryProtocol.Factory());
-	sargs.transportFactory(new TFramedTransport.Factory());
-	sargs.processorFactory(new TProcessorFactory(processor));
-	TServer server = new THsHaServer(sargs);
-	server.serve();
+		// launch Thrift server
+		BcryptService.Processor processor = new BcryptService.Processor<BcryptService.Iface>(new BcryptServiceHandler());
+		TNonblockingServerSocket socket = new TNonblockingServerSocket(portFE);
+		THsHaServer.Args sargs = new THsHaServer.Args(socket);
+		sargs.protocolFactory(new TBinaryProtocol.Factory());
+		sargs.transportFactory(new TFramedTransport.Factory());
+		sargs.processorFactory(new TProcessorFactory(processor));
+		TServer server = new THsHaServer(sargs);
+		server.serve();
     }
 }
