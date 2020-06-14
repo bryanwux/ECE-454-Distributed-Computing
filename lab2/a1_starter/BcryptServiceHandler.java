134031d6e0fc0285d168c50841f2dee3216fea13
Difei Gu <gudifeifriend@gmail.com>
2020-06-14 14:00:28 -0400

    bug fix

--- lab2/a1_starter/BcryptServiceHandler.java
+++ lab2/a1_starter/BcryptServiceHandler.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Iterator;
+import java.util.Map;
 
 import org.apache.http.impl.cookie.BasicClientCookie;
 import org.apache.log4j.Logger;
@@ -8,11 +10,77 @@ import java.util.concurrent.*;
 import org.apache.thrift.protocol.TProtocolFactory;
 import org.mindrot.jbcrypt.BCrypt;
 
-abstract class BcryptServiceHandler {
-	List<String> hashPassword(List<String> password, short logRounds);
-	List<Boolean> checkPassword(List<String> password, List<String> hash);
-	void BENodeHandlerInit(String BEHost, int BEPort);
+import org.apache.thrift.async.TAsyncClientManager;
+import org.apache.thrift.protocol.TCompactProtocol;
+import org.apache.thrift.protocol.TProtocolFactory;
+import org.apache.thrift.transport.TNonblockingSocket;
+import org.apache.thrift.transport.TNonblockingTransport;
 
+
+class TransportPair{
+	private BcryptService.AsyncClient client;
+	private TNonblockingTransport transport;
+
+	public TransportPair(BcryptService.AsyncClient client, TNonblockingTransport transport){
+		this.client = client;
+		this.transport = transport;
+	}
+
+	public BcryptService.AsyncClient getClient(){
+		return this.client;
+	}
+
+	public TNonblockingTransport getTransport(){
+		return this.transport;
+	}
+}
+
+class BackendNode {
+	private String BEHost;
+	private int BEPort;
+	private int RequestNum;//what is this??
+	private ConcurrentHashMap<TransportPair, Boolean> ClientTransportPair;
+
+	BackendNode(String BEHost, int BEPort, ConcurrentHashMap<TransportPair, Boolean> ClientTransportPair) {
+		this.BEHost = BEHost;
+		this.BEPort = BEPort;
+		this.RequestNum = 0;
+		this.ClientTransportPair = ClientTransportPair;
+	}
+
+	public String getBEHost() {
+		return this.BEHost;
+	}
+
+	public int getBEPort() {
+		return BEPort;
+	}
+
+	public synchronized int getRequestNum() {
+		return this.RequestNum;
+	}
+
+	public synchronized void incrementRequest() {
+		this.RequestNum++;
+	}
+
+	public synchronized void decrementRequest(TransportPair pair) {
+		this.RequestNum--;
+		// no one is using this client
+		this.ClientTransportPair.put(pair, false);
+	}
+
+	public synchronized TransportPair getTransportPair() {
+		Iterator ctIterator = ClientTransportPair.entrySet().iterator();
+		while (ctIterator.hasNext()) {
+			Map.Entry entry = (Map.Entry) ctIterator.next();
+			if ((Boolean) entry.getValue() == false) {
+				ClientTransportPair.put((TransportPair) entry.getKey(), true);
+				return (TransportPair) entry.getKey();
+			}
+		}
+		return null;
+	}
 }
 
 public class BcryptServiceHandler implements BcryptService.Iface {
@@ -63,4 +131,28 @@ public class BcryptServiceHandler implements BcryptService.Iface {
 	    throw new IllegalArgument(e.getMessage());
 	}
     }
+
+	public void BENodeHandlerInit(String BEHost, int BEPort) throws IllegalArgument, org.apache.thrift.TException {
+		try {
+			TProtocolFactory protocolFactory;
+			TAsyncClientManager clientManager;
+			TNonblockingTransport transport;
+
+			ConcurrentHashMap<TransportPair, Boolean> ClientTransportPair = new ConcurrentHashMap<TransportPair, Boolean>();
+			for(int i=0; i<4; i++){
+				protocolFactory = new TCompactProtocol.Factory();
+				clientManager = new TAsyncClientManager();
+				transport = new TNonblockingSocket(BEHost, BEPort);
+
+				BcryptService.AsyncClient client = new BcryptService.AsyncClient(protocolFactory, clientManager, transport);
+				TransportPair pair = new TransportPair(client, transport);
+				ClientTransportPair.put(pair, false);  // set backend node to busy
+			}
+
+			BackendNode BENode = new BackendNode(BEHost, BEPort, ClientTransportPair);
+			backendNodes.add(BENode);
+		} catch (Exception e) {
+
+		}
+	}
 }
