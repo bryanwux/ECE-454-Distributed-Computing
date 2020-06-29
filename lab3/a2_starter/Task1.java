7916fac134130115810ba2301ba6805338d6d484
4everZard <bryanwu610@gmail.com>
2020-06-29 16:21:27 -0400

    task1 hadoop

--- lab3/a2_starter/Task1.java
+++ lab3/a2_starter/Task1.java
@@ -12,23 +12,34 @@ import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
-import javafx.scene.text.*;
-
 public class Task1 {
   public static class MovieRatingMapper extends Mapper<Object, Text, Text, IntWritable>{
     private Text movieName = new Text();
-    private Text ratings = new Text();
-    public void map(Object key, Text value, Context context) throws IOException, InterruptedException{
-    
+    private Text maxIndex = new Text();
 
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException{
+      String[] tokens = value.toString().split(",");
+      movieName.set(tokens[0]);
+      int currentMaxRating = Integer.MIN_VALUE;
+
+      for(i=1, i<tokens.length,i++){
+        if(tokens[i].isEmpty())
+          continue;
+        int rating = Integer.parsetInt(ratings[i]);
+        if(rating > currentMaxRating){
+          currentMaxRating = rating;
+          maxIndex.clear();
+          maxIndex.set(String.valueOf(i))
+        }else if(rating == currentMaxRating){
+          byte[] bytes = String.format(",%s", i).getBytes();
+          maxIndex.append(bytes, 0, bytes.length);
+        }
+      }
+      context.write(movieName, maxIndex);
     
     }
 
   }
-
-
-  public static
-
 
     
     
@@ -42,12 +53,6 @@ public class Task1 {
     String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
     
     // add code here
-    job.setMapperClass(MovieRatingMapper.class);
-    job.setCombinerClass(MovieRatingCombiner.class);
-    job.setReducerClass(MovieRatingReducer.class);
-
-    job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(IntWritable.class);
 
     TextInputFormat.addInputPath(job, new Path(otherArgs[0]));
     TextOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
