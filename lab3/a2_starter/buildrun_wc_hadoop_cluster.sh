ea8e6b5f5fb39fc1b782834088b603f719b2e042
Difei Gu <gudifeifriend@gmail.com>
2020-06-29 16:33:31 -0400

    change bash script

--- lab3/a2_starter/buildrun_wc_hadoop_cluster.sh
+++ lab3/a2_starter/buildrun_wc_hadoop_cluster.sh
@@ -12,25 +12,25 @@ export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop/
 export CLASSPATH=`$HADOOP_HOME/bin/hadoop classpath`
 
 echo --- Deleting
-rm HadoopWC.jar
-rm HadoopWC*.class
+rm Task1.jar
+rm Task1*.class
 
 echo --- Compiling
-$JAVA_HOME/bin/javac HadoopWC.java
+$JAVA_HOME/bin/javac Task1.java
 if [ $? -ne 0 ]; then
     exit
 fi
 
 echo --- Jarring
-$JAVA_HOME/bin/jar -cf HadoopWC.jar HadoopWC*.class
+$JAVA_HOME/bin/jar -cf Task1.jar Task1*.class
 
 echo --- Running
-INPUT=/a2_inputs/wc_input.txt
+INPUT=/a2_inputs/smalldata.txt
 OUTPUT=/user/${USER}/a2_starter_code_output_hadoop/
 
 $HADOOP_HOME/bin/hdfs dfs -rm -R $OUTPUT
 #$HADOOP_HOME/bin/hdfs dfs -copyFromLocal sample_input/smalldata.txt /user/${USER}/
-time $HADOOP_HOME/bin/yarn jar HadoopWC.jar HadoopWC -D mapreduce.map.java.opts=-Xmx4g $INPUT $OUTPUT
+time $HADOOP_HOME/bin/yarn jar Task1.jar Task1 -D mapreduce.map.java.opts=-Xmx4g $INPUT $OUTPUT
 
 $HADOOP_HOME/bin/hdfs dfs -ls $OUTPUT
 $HADOOP_HOME/bin/hdfs dfs -cat $OUTPUT/*
