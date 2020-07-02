02650db637bc037fbeee935c7b5cab792c8a9c8f
4everZard <bryanwu610@gmail.com>
2020-07-02 03:31:08 -0400

    update Task4 hadoop

--- lab3/a2_starter/Task4.java
+++ lab3/a2_starter/Task4.java
@@ -1,6 +1,8 @@
 import java.io.IOException;
 import java.util.StringTokenizer;
-
+import java.io.*;
+import java.net.URI;
+import java.util.*;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
@@ -16,43 +18,54 @@ public class Task4 {
   public static class MovieSimilarityMapper extends Mapper<Object, Text, Text, IntWritable>{
     private Text movieName;
     private IntWritable movieSimilarity; 
-  
+    private List<String> cache;
   // called once at the beginning of each task
   @Override
   public void setup(Context context)
     throws IOException, InterruptedException{
-      BufferedReader reader = new BufferedReader(new FileReader(DISTRIBUTED_CACHE_LABEL));
+      // 
+      Path cahcedInput = context.getLocalCacheFiles()[0];
+      BufferedReader reader = new BufferedReader(new FileReader(cahcedInput.toString()));
       cache.clear();
       String line;
-      while ((line = reader.readLine()) != null) cache.add(line);
+      while ((line = reader.readLine()) != null) 
+        cache.add(line);
   }
 
   public void map(Object key, Text value, Context context)
     throws IOException, InterruptedException{
-      String[] tokens = value.toString().split(",");
-      String title = tokens[0];
-      for (String cacheLine : cache) {
-        final String[] tokensB = cacheLine.split(",", -1);
-        final String titleB = tokensB[0];
+      String[] token1 = value.toString().split(",");
+      String title1 = token1[0];
+      for (String c : cache) {
+        String[] token2 = c.split(",");
+        String title2 = token2[0];
 
         // maintain lexicographic order between titles
-        if (titleA.compareTo(titleB) >= 0) continue;
+        if (titleA.compareTo(titleB) < 0)
+          int similarity = computeSimilarity(token1,token2)
+        else
+          continue;
 
-        int count = 0;
-        for (int i = 1; i < tokensA.length; i++) {
-            String a = tokensA[i];
-            String b = tokensB[i];
-            if (!a.isEmpty() && !b.isEmpty() && a.equals(b)) {
-                count++;
-            }
-        }
-        titlePair.set(titleA + "," + titleB);
-        similarity.set(count);
+      
+        titlePair.set(title1 + "," + title2);
+        similarity.set(similarity);
         context.write(titlePair, similarity);
       }
     }
+
+    public int computeSimilarity(String[] token1, String[] token2){
+      int counter = 0;
+      for (int i=1; i<token1.length; i++){
+        String rating1 = token1[i];
+        String rating2 = token2[i];
+        if(rating1 == rating2 && !rating1.isEmpty() && ! rating2.isEmpty()){
+          counter++;
+        }
+      }
+      return counter;
+    }
   }
-  
+
   public static void main(String[] args) throws Exception {
     Configuration conf = new Configuration();
     conf.set("mapreduce.output.textoutputformat.separator", ",");
@@ -61,9 +74,12 @@ public class Task4 {
     job.setJarByClass(Task4.class);
 
     String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
-    System.out.println(otherArgs);
-    // add code here
-    job.setMapperClass(Task4.class);
+    
+    // make the input file accessible to all the mappers for faster access of the data
+    job.addCacheFile(new URI(otherArgs[0]));
+    job.setNumReduceTasks(0);
+
+    job.setMapperClass(MovieSimilarityMapper.class);
     job.setMapOutputKeyClass(Text.class);
     job.setMapOutputValueClass(IntWritable.class);
 
