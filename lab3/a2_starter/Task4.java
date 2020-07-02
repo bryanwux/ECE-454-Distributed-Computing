576ba4322da430d473a2a214fa9ea3d8d883d56a
4everZard <bryanwu610@gmail.com>
2020-07-02 04:21:56 -0400

    e

--- lab3/a2_starter/Task4.java
+++ lab3/a2_starter/Task4.java
@@ -1,94 +1,179 @@
-import java.io.IOException;
-import java.util.StringTokenizer;
+// import java.io.IOException;
+// import java.util.StringTokenizer;
+// import java.io.*;
+// import java.net.URI;
+// import java.util.*;
+// import org.apache.hadoop.conf.Configuration;
+// import org.apache.hadoop.fs.Path;
+// import org.apache.hadoop.io.IntWritable;
+// import org.apache.hadoop.io.Text;
+// import org.apache.hadoop.mapreduce.Job;
+// import org.apache.hadoop.mapreduce.Mapper;
+// import org.apache.hadoop.mapreduce.Reducer;
+// import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+// import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+// import org.apache.hadoop.util.GenericOptionsParser;
+
+// public class Task4 {
+//   public static class MovieSimilarityMapper extends Mapper<Object, Text, Text, IntWritable>{
+//     private Text movieName = new Text();
+//     private IntWritable movieSimilarity = new IntWritable(); 
+//     private List<String> cache = new LinkedList<>();
+//   // called once at the beginning of each task
+//   @Override
+//   public void setup(Context context)
+//     throws IOException, InterruptedException{
+//       // cache input text file
+//       Path cahcedInput = context.getLocalCacheFiles()[0];
+//       BufferedReader reader = new BufferedReader(new FileReader(cahcedInput.toString()));
+//       cache.clear();
+//       String line;
+//       while ((line = reader.readLine()) != null) 
+//         cache.add(line);
+//   }
+
+//   public void map(Object key, Text value, Context context)
+//     throws IOException, InterruptedException{
+//       String[] token1 = value.toString().split(",");
+//       String title1 = token1[0];
+//       for (String c : cache) {
+//         String[] token2 = c.split(",");
+//         String title2 = token2[0];
+
+//         // maintain lexicographic order between titles
+//         if (title1.compareTo(title2) < 0){
+//           int similarity = computeSimilarity(token1,token2);
+//           movieName.set(title1 + "," + title2);
+//           movieSimilarity.set(similarity);
+//           context.write(movieName, movieSimilarity);
+//         }
+//         else
+//           continue;
+//       }
+//     }
+
+//     public int computeSimilarity(String[] token1, String[] token2){
+//       int counter = 0;
+//       for (int i=1; i<token1.length; i++){
+//         String rating1 = token1[i];
+//         String rating2 = token2[i];
+//         if(rating1 == rating2 && !rating1.isEmpty() && ! rating2.isEmpty()){
+//           counter++;
+//         }
+//       }
+//       return counter;
+//     }
+//   }
+
+//   public static void main(String[] args) throws Exception {
+//     Configuration conf = new Configuration();
+//     conf.set("mapreduce.output.textoutputformat.separator", ",");
+    
+//     Job job = Job.getInstance(conf, "Task4");
+//     job.setJarByClass(Task4.class);
+
+//     String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+    
+//     // make the input file accessible to all the mappers for faster access of the data
+//     job.addCacheFile(new URI(otherArgs[0]));
+//     job.setNumReduceTasks(0);
+
+//     job.setMapperClass(MovieSimilarityMapper.class);
+//     job.setMapOutputKeyClass(Text.class);
+//     job.setMapOutputValueClass(IntWritable.class);
+
+//     TextInputFormat.addInputPath(job, new Path(otherArgs[0]));
+//     TextOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+    
+//     System.exit(job.waitForCompletion(true) ? 0 : 1);
+//   }
+// }
+
 import java.io.*;
 import java.net.URI;
 import java.util.*;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
 public class Task4 {
-  public static class MovieSimilarityMapper extends Mapper<Object, Text, Text, IntWritable>{
-    private Text movieName = new Text();
-    private IntWritable movieSimilarity = new IntWritable(); 
-    private List<String> cache = new LinkedList<>();
-  // called once at the beginning of each task
-  @Override
-  public void setup(Context context)
-    throws IOException, InterruptedException{
-      // cache input text file
-      Path cahcedInput = context.getLocalCacheFiles()[0];
-      BufferedReader reader = new BufferedReader(new FileReader(cahcedInput.toString()));
-      cache.clear();
-      String line;
-      while ((line = reader.readLine()) != null) 
-        cache.add(line);
-  }
 
-  public void map(Object key, Text value, Context context)
-    throws IOException, InterruptedException{
-      String[] token1 = value.toString().split(",");
-      String title1 = token1[0];
-      for (String c : cache) {
-        String[] token2 = c.split(",");
-        String title2 = token2[0];
+    /*
+        The Lord of The Rings,5,4,4,,3,2
+        Apocalypto,3,5,4,,5,4
+        Apollo 13,,,4,5,,5
+     */
 
-        // maintain lexicographic order between titles
-        if (title1.compareTo(title2) < 0){
-          int similarity = computeSimilarity(token1,token2);
-          System.out.println(token1);
-          System.out.println(token1);
-          System.out.println(similarity);
+    private static final String DISTRIBUTED_CACHE_LABEL = "cachedInput";
 
-          movieName.set(title1 + "," + title2);
-          movieSimilarity.set(similarity);
-          context.write(movieName, movieSimilarity);
+    public static class SimilarityMapper extends Mapper<Object, Text, Text, IntWritable> {
+
+        private final Text titlePair = new Text();
+        private final IntWritable similarity = new IntWritable();
+
+        private final List<String> cache = new LinkedList<>();
+
+        @Override
+        protected void setup(Context context) throws IOException {
+            BufferedReader reader = new BufferedReader(new FileReader(DISTRIBUTED_CACHE_LABEL));
+            cache.clear();
+            String line;
+            while ((line = reader.readLine()) != null) cache.add(line);
         }
-        else
-          continue;
-      }
+
+        @Override
+        protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            final String[] tokensA = value.toString().split(",", -1);
+            final String titleA = tokensA[0];
+            for (String cacheLine : cache) {
+                final String[] tokensB = cacheLine.split(",", -1);
+                final String titleB = tokensB[0];
+
+                // maintain lexicographic order between titles
+                if (titleA.compareTo(titleB) >= 0) continue;
+
+                int count = 0;
+                for (int i = 1; i < tokensA.length; i++) {
+                    String a = tokensA[i];
+                    String b = tokensB[i];
+                    if (!a.isEmpty() && !b.isEmpty() && a.equals(b)) {
+                        count++;
+                    }
+                }
+                titlePair.set(titleA + "," + titleB);
+                similarity.set(count);
+                context.write(titlePair, similarity);
+            }
+        }
     }
 
-    public int computeSimilarity(String[] token1, String[] token2){
-      int counter = 0;
-      for (int i=1; i<token1.length; i++){
-        String rating1 = token1[i];
-        String rating2 = token2[i];
-        if(rating1 == rating2 && !rating1.isEmpty() && ! rating2.isEmpty()){
-          counter++;
-        }
-      }
-      return counter;
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        conf.set("mapreduce.output.textoutputformat.separator", ",");
+
+        String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+
+        Job job = Job.getInstance(conf, "Task4");
+        job.setJarByClass(Task4.class);
+
+        // Add the input to the distributed cache so each worker has a local cached copy
+        job.addCacheFile(new URI(otherArgs[0] + "#" + DISTRIBUTED_CACHE_LABEL));
+
+        job.setMapperClass(SimilarityMapper.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+
+        TextInputFormat.addInputPath(job, new Path(otherArgs[0]));
+        TextOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
-  }
-
-  public static void main(String[] args) throws Exception {
-    Configuration conf = new Configuration();
-    conf.set("mapreduce.output.textoutputformat.separator", ",");
-    
-    Job job = Job.getInstance(conf, "Task4");
-    job.setJarByClass(Task4.class);
-
-    String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
-    
-    // make the input file accessible to all the mappers for faster access of the data
-    job.addCacheFile(new URI(otherArgs[0]));
-    job.setNumReduceTasks(0);
-
-    job.setMapperClass(MovieSimilarityMapper.class);
-    job.setMapOutputKeyClass(Text.class);
-    job.setMapOutputValueClass(IntWritable.class);
-
-    TextInputFormat.addInputPath(job, new Path(otherArgs[0]));
-    TextOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
-    
-    System.exit(job.waitForCompletion(true) ? 0 : 1);
-  }
 }
+
