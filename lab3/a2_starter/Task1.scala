d1bce3bd5ad6cf6cab0cd090a3c0e28f21dd6c76
4everZard <bryanwu610@gmail.com>
2020-07-02 20:30:34 -0400

    emm

--- lab3/a2_starter/Task1.scala
+++ lab3/a2_starter/Task1.scala
@@ -5,12 +5,22 @@ object Task1 {
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("Task 1")
     val sc = new SparkContext(conf)
-
+    // RDD[String]
     val textFile = sc.textFile(args(0))
+    // RDD[Array[String]]
+    val line = textFile.map(line => line.split("\t"))
+
+    val rdd = line.flatMap( arr=>{
+      val movieName = arr(0)
+      //val maxRatingIndex = arr(1)
+      
+    }
+
+    )
 
     // modify this code
-    val output = textFile.map(x => x);
-    
+    //val output = textFile.map(x => x)
+    val output = rdd
     output.saveAsTextFile(args(1))
   }
 }
