b90b346a39b5e64aae78dd7e224a7a1ea0b4280a
Difei Gu <gudifeifriend@gmail.com>
2020-07-05 21:19:13 -0400

    Added task2 hedoop

--- lab3/a2_starter/Task2.java
+++ lab3/a2_starter/Task2.java
@@ -14,9 +14,35 @@ import org.apache.hadoop.util.GenericOptionsParser;
 
 public class Task2 {
 
-  // add code here
+  public static class TokenizerMapper extends Mapper<Object, Text, IntWritable>{
+    private final static intWritable one = new IntWritable(1);
 
-    
+    public void map(Object key, Text value, Context context)
+            throws IOException, InterruptedException{
+      String[] tokens = value.toString().split(",");
+
+      for(int i=1; i<tokens.length; i++) {
+        if(tokens[i].isEmpty())
+          continue;
+        context.write(one);
+      }
+    }
+  }
+
+  public static class IntSumReducer extends Reducer<Text,IntWritable,IntWritable>{
+    private IntWritable result=new IntWritable();
+
+    public void reduce(Text key,iterable<IntWritable> values,Context context)
+      throws IOException,InterruptedException
+    {
+      int sum=0;
+      for(IntWritable val : values){
+        sum+=val.get();
+      }
+      result.set(sum);
+      context.write(result);
+    }
+  }
     
   public static void main(String[] args) throws Exception {
     Configuration conf = new Configuration();
@@ -27,7 +53,9 @@ public class Task2 {
 
     String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
 
-    // add code here
+    job.setMapperClass(TokenizerMapper.class);
+    job.setReducerClass(IntSumReducer.class);
+
 
     TextInputFormat.addInputPath(job, new Path(otherArgs[0]));
     TextOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
