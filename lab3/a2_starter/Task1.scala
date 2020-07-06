d6a15cdd6c835bacfb3944e1fa4f1b9ed47d00db
Difei Gu <gudifeifriend@gmail.com>
2020-07-06 12:17:54 -0400

    test

--- lab3/a2_starter/Task1.scala
+++ lab3/a2_starter/Task1.scala
@@ -13,8 +13,8 @@ object Task1 {
 
     for(line <- token){
         println(line.toString)
-        val segs = line.toString.map(x => x.split(","))
-        println(segs.toString)
+        //val segs = line.toString.map(x => x.split(","))
+        //println(segs.toString)
         val movie_name = segs.first()
         println(movie_name.toString)
         //val rating_with_index_sorted = segs.zipWithIndex.mapPartitionsWithIndex((index, it) => if (index == 0) it.drop(1) else it,preservesPartitioning = true).sortByKey(false).map((r,i) => (i,r))
