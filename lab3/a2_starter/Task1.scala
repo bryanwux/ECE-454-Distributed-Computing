6fd4927817c49576febf62fdecde9382445f5414
Difei Gu <gudifeifriend@gmail.com>
2020-07-06 15:30:31 -0400

    Complete task1 scala

--- lab3/a2_starter/Task1.scala
+++ lab3/a2_starter/Task1.scala
@@ -7,9 +7,7 @@ object Task1 {
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("Task 1")
     val sc = new SparkContext(conf)
-    // RDD[String]
     val textFile = sc.textFile(args(0))
-    // RDD[Array[String]]
 
     val result = textFile.map(x => {
       var rating = x.split(",");
@@ -29,32 +27,7 @@ object Task1 {
       }
       movie_name+","+user_max.mkString(",");
     })
-
-
-    //val rating_with_index_sorted = segs.zipWithIndex.mapPartitionsWithIndex((index, it) => if (index == 0) it.drop(1) else it,preservesPartitioning = true).sortByKey(false).map((r,i) => (i,r))
-    //println(rating_with_index_sorted)
-    //val highest = rating_with_index_sorted.filter(f=>if(f._2-rating_with_index_sorted.first()._2==0) true else false).map((i,r)=>i)
-    //println(highest)
-    //val result = movie_name.union(highest).collect()
-    //val rdd = rdd.union(result)
-
-    //filter(f=>if(f._2-rating_with_index_sorted.first()._2==0) true else false).map((i,r)=>i)
-    //val movie_name = token.first()
-
-    //val rating = token.zipWithIndex.mapPartitionsWithIndex((index, it) => if (index == 0) it.drop(1) else it,preservesPartitioning = true)
-    //println(rating.toString)
-    //val rating_with_index_sorted = rating_t.sortByKey(False).map((r,i) => (i,r))
-    //println(rating_with_index_sorted)
-    //val highest = rating_with_index_sorted.filter(f=>if(f._2-rating_with_index_sorted.first()._2==0) true else false).map((i,r)=>i)
-
-    //println(highest)
-
-    //val rdd = result
-    //val rdd = movie_name.union(highest)
-
     // modify this code
-    //val output = textFile.map(x => x)
-    println(result)
     val output = result
     output.saveAsTextFile(args(1))
   }
