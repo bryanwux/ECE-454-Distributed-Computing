574cb4243d910882a5b8274f443de8e67e91ac7b
Jiayu <j285wu@uwaterloo.ca>
2020-07-06 15:07:36 -0400

    task3 scala

--- lab3/a2_starter/Task3.scala
+++ lab3/a2_starter/Task3.scala
@@ -2,6 +2,7 @@ import org.apache.spark.{SparkContext, SparkConf}
 
 // please don't change the object name
 object Task3 {
+
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("Task 3")
     val sc = new SparkContext(conf)
@@ -9,8 +10,19 @@ object Task3 {
     val textFile = sc.textFile(args(0))
 
     // modify this code
-    val output = textFile.map(x => x);
-    
+
+    def mapIdtoRating(line: String): Array[(Int,Int)] = {
+        line.split(",", -1)
+            .zipWithIndex
+            .drop(1)
+            .filter(_.nonEmpty)
+            .map{case(_,idx) => (idx,1)}
+
+    }
+
+    val output = textFile.flatMap(mapIdtoRating)
+                         .reduceByKey(_+_)
+                         .map(x => x._1 + "," + x._2)
     output.saveAsTextFile(args(1))
   }
 }
