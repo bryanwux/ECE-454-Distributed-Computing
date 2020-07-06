921d0d693cc63cdb48360f479f5fedb74c6f0bf8
Difei Gu <gudifeifriend@gmail.com>
2020-07-06 12:50:24 -0400

    test

--- lab3/a2_starter/Task1.scala
+++ lab3/a2_starter/Task1.scala
@@ -11,13 +11,14 @@ object Task1 {
     // RDD[Array[String]]
     val token = textFile.map(line => line.split("\n")).collect
 
+    val rating_with_index_sorted = Null
     for(line <- token){
         println(line)
-        val segs = line.map(x => x.split(",")).collect
+        val segs = line.map(x => x.split(","))
         println(segs)
         val movie_name = segs.first()
         println(movie_name.toString)
-        val rating_with_index_sorted = segs.zipWithIndex.mapPartitionsWithIndex((index, it) => if (index == 0) it.drop(1) else it,preservesPartitioning = true).sortByKey(false).map((r,i) => (i,r))
+        rating_with_index_sorted = segs.zipWithIndex.mapPartitionsWithIndex((index, it) => if (index == 0) it.drop(1) else it,preservesPartitioning = true).sortByKey(false).map((r,i) => (i,r))
         println(rating_with_index_sorted)
         //val highest = rating_with_index_sorted.filter(f=>if(f._2-rating_with_index_sorted.first()._2==0) true else false).map((i,r)=>i)
         //println(highest)
