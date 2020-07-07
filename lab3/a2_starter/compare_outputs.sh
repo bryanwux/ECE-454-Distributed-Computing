e86f8d545e28d382d29eb236c3b2474fc3185ef5
Jiayu <j285wu@uwaterloo.ca>
2020-07-06 20:08:37 -0400

    update bash script

--- lab3/a2_starter/compare_outputs.sh
+++ lab3/a2_starter/compare_outputs.sh
@@ -9,13 +9,44 @@
 hdfs dfs -cat task1-hadoop.out.txt/* | sort > normalized_spark.txt
 hdfs dfs -cat task1-spark.out.txt/* | sort > normalized_hadoop.txt
 
-echo Diffing Spark and Hadoop outputs:
+echo Diffing Spark and Hadoop task1 outputs:
 diff normalized_spark.txt normalized_hadoop.txt
 
 if [ $? -eq 0 ]; then
-    echo Outputs match.
+    echo Task1 Outputs match.
 else
-    echo Outputs do not match. Looks for bugs.
+    echo Task1 Outputs do not match. Looks for bugs.
+fi
+
+rm normalized_spark.txt
+rm normalized_hadoop.txt
+
+hdfs dfs -cat task2-hadoop.out.txt/* | sort > normalized_spark.txt
+hdfs dfs -cat task2-spark.out.txt/* | sort > normalized_hadoop.txt
+
+echo Diffing Spark and Hadoop task2 outputs:
+diff normalized_spark.txt normalized_hadoop.txt
+
+if [ $? -eq 0 ]; then
+    echo Task2 Outputs match.
+else
+    echo Task2 Outputs do not match. Looks for bugs.
+fi
+
+rm normalized_spark.txt
+rm normalized_hadoop.txt
+
+
+hdfs dfs -cat task3-hadoop.out.txt/* | sort > normalized_spark.txt
+hdfs dfs -cat task3-spark.out.txt/* | sort > normalized_hadoop.txt
+
+echo Diffing Spark and Hadoop task3 outputs:
+diff normalized_spark.txt normalized_hadoop.txt
+
+if [ $? -eq 0 ]; then
+    echo Task3 Outputs match.
+else
+    echo Task3 Outputs do not match. Looks for bugs.
 fi
 
 rm normalized_spark.txt
