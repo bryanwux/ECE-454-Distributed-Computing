8f250efecf7a224c0fcb80ef47a4b56c7e6f2459
Jiayu <j285wu@uwaterloo.ca>
2020-07-20 18:23:19 -0400

    update

--- lab4/a3_starter/KeyValueHandler.java
+++ lab4/a3_starter/KeyValueHandler.java
@@ -1,6 +1,10 @@
 import java.net.InetSocketAddress;
 import java.util.*;
 import java.util.concurrent.*;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import com.google.common.util.concurrent.Striped;
 
 import org.apache.thrift.*;
 import org.apache.thrift.server.*;
@@ -12,9 +16,11 @@ import org.apache.zookeeper.data.*;
 import org.apache.curator.*;
 import org.apache.curator.retry.*;
 import org.apache.curator.framework.*;
-
+import org.apache.curator.framework.api.CuratorWatcher;
 
 public class KeyValueHandler implements KeyValueService.Iface, CuratorWatcher {
+    public final int LOCK_NUM = 64;
+
     private Map<String, String> myMap;
     private CuratorFramework curClient;
     private String zkNode;
@@ -23,6 +29,9 @@ public class KeyValueHandler implements KeyValueService.Iface, CuratorWatcher {
 
     private volatile InetSocketAddress primaryAddress;
     private volatile InetSocketAddress backupAddress;
+    private ReentrantLock reLock = new ReentrantLock();
+    private Striped<Lock> stripedLock = Striped.lock(LOCK_NUM);
+    private volatile ConcurrentLinkedQueue<KeyValueService.Client> backupPool = null;
 
     public KeyValueHandler(String host, int port, CuratorFramework curClient, String zkNode) {
 	this.host = host;
@@ -36,6 +45,17 @@ public class KeyValueHandler implements KeyValueService.Iface, CuratorWatcher {
     
     curClient.sync();
     List<String> children = curClient.getChildren().usingWatcher(this).forPath(zkNode);
+    if(children.size() > 1){
+        //sort the return list of children in ascending lexicographic order
+        Collections.sort(children);
+        byte[] data = curClient.getData().forPath(zkNode + "/" + children.get(children.size() - 1));
+        String strBackup = new String(data);
+        String[] backup = strBackup.split(":");
+        String backupHost = backup[0];
+        int backupPort = Integer.parseInt(backup[1]);
+    }
+
+    
     }
 
     // basically read operation, do not need locks
@@ -62,6 +82,26 @@ public class KeyValueHandler implements KeyValueService.Iface, CuratorWatcher {
         if (!isPrimary()){
             throw new org.apache.thrift.TException("Backup znode can not get");
         }else{
+            // Returns the stripe that corresponds to the passed key
+            Lock lock = stripedLock.get(key);
+            lock.lock();
+
+            if (!reLock.isLocked()){
+                try{
+                    myMap.put(key, value);
+                    if(!this.backupPool.isEmpty()){
+                        // retrieves the head of the backupPol
+                        KeyValueService.Client backupClient = backupPool.poll();
+                        backupClient.putBackup(key,value);
+                        this.backupPool.offer(backupClient);
+                    }
+                }catch(Exception e){
+                    e.printStackTrace();
+                    this.backupPool = null;
+                }finally{
+                    lock.unlock();
+                }
+            }
 
         }
     }
