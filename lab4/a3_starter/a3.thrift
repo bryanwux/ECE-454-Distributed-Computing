77a7ab87e6fec1653576e40829c8e3d9e8f65727
Jiayu <j285wu@uwaterloo.ca>
2020-07-24 11:51:44 -0400

    thrift

--- lab4/a3_starter/a3.thrift
+++ lab4/a3_starter/a3.thrift
@@ -1,4 +1,8 @@
 service KeyValueService {
   string get(1: string key);
   void put(1: string key, 2: string value);
+  void backupPut(1: string key, 2: string value);
+  void setPrimary(1: bool isPrimary);
+  void sync(1: map<string, string> data);
+
 }
